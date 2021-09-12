87fbd63ec636fd95820d9e9a502ea164c389581a
hro-maker <hrant.muradyan.1998@mail.ru>
2021-09-12 10:45:46 +0400

    feat: add hanoi-tower solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ export default function createDreamTeam( members ) {
+  let x = "";
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  members = members.filter(name => typeof(name) == "string");
+  for (let i = 0; i < members.length; i++) {
+    members[i] = members[i].trim();
+    x += members[i].substring(0,1);
+  }
+  return x.toLowerCase().split("").sort((a,b) => a.localeCompare(b)).join("").toUpperCase();
 }
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,32 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ export default function repeater( str, options ) {
+  str = String(str);
+  let additional = "";
+  let newStr = "";
+  if (!options.repeatTimes) {
+    options.repeatTimes = 1;
+  }
+  if (!options.separator) {
+    options.separator = "+";
+  }
+  if (options.addition === undefined) {
+    options.addition = "";
+  }
+  if (!options.additionRepeatTimes) {
+    options.additionRepeatTimes = 1;
+  }
+  if (!options.additionSeparator) {
+    options.additionSeparator = "|";
+  }
+  for (let i = 1; i < options.additionRepeatTimes; i++) {
+    additional += options.addition + options.additionSeparator;
+  }
+  additional += options.addition;
+  for (let j = 1; j < options.repeatTimes; j++) {
+    newStr += str + additional + options.separator;
+  }
+  newStr += str + additional;
+  return newStr;
 }
